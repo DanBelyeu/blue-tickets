0b2502c1ec52ee6c78ed57a8993b4cc7867e72f1
Dan B <daniel.belyeu@gmail.com>
2025-03-19 23:48:34 -0400

    Add API calls for ticket functions

--- src/app/ticket-card.js
+++ src/app/ticket-card.js
@@ -1,21 +1,53 @@
 "use client";
 // Function component with state using useState hook
-import React, { useState }  from 'react';
-import styles from "./page.module.css";
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid2';
+import React, { useState, useEffect }  from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 
 
+const updateBackend = (name, count) => {
+    return fetch('http://localhost:3001/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify({name, count})})
+    .then(response =>{
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+    .catch(error => {
+        console.error("There was an error:", error);
+    })
+};
+
+const getCount = (name) => {
+    const url = `http://localhost:3001/ticketCount/${name}`;
+    return fetch(url)
+    .then(response =>{
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .catch(error => {
+          console.error("There was an error:", error);
+        });    
+}
+
 function TicketCard(props) {
     const { name } = props;
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState();
+
+    useEffect(() => {
+        setCount(getCount(name))
+    }, []);
 
     const handleClick = () => {
-        setCount(count + 1);
+        setCount(updateBackend(name, count));
     };
 
     return (
